79f74e4c3bbabcbd4656db6c0d4251ba35a379ed
ElynaRia <164609161+ElynaRia@users.noreply.github.com>
2024-03-29 18:56:59 +0700

    Delete config cors

--- BE/index.js
+++ BE/index.js
@@ -16,11 +16,7 @@ async function koneksi() { await mongoose.connect(API) };
 
 
 // middleware
-app.use(cors({
-    origin: [process.env.CORS_URL],
-    credentials: true,
-    methods: ["GET", "POST"]
-}));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded());
 app.use("/api", router);
