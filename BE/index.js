18e38e52ae39b33e7c6a621c372e4f635c099d1a
ElynaRia <elynaria28@gmail.com>
2024-03-29 21:24:35 +0700

    1212312

--- BE/index.js
+++ BE/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 const router = require('./Controller/Route');
+const bodyParser = require('body-parser');
 
 
 
@@ -17,8 +18,8 @@ async function koneksi() { await mongoose.connect(API) };
 
 // middleware
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded());
+app.use(bodyParser.json({ limit: "10mb" }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use("/api", router);
 
 
